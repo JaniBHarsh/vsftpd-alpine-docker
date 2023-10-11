84ed29e87a03e10c3fea138e3841744698a11c62
harsh <harshjani88@gmail.com>
2023-10-11 16:33:34 +0530

    added deploy script for production

---
+++ deplyo.sh
@@ -0,0 +1,9 @@
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 561872739683.dkr.ecr.us-east-2.amazonaws.com 
+
+docker pull 561872739683.dkr.ecr.us-east-2.amazonaws.com/vsftpd
+
+docker tag  561872739683.dkr.ecr.us-east-2.amazonaws.com/vsftpd vsftpd 
+
+docker-compose -f ~/vsftpd/docker-compose.yml down 
+
+docker-compose -f ~/vsftpd/docker-compose.yml up -d
